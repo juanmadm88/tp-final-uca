e43f615bce4ff193480251afa632258cc3d3adcb
Juan Martin Di Martino <juan.dimartino@intive.com>
2023-12-28 14:01:14 -0300

    mas tests

--- src/authentication/auth.controller.spec.ts
+++ src/authentication/auth.controller.spec.ts
@@ -32,10 +32,28 @@ describe('AuthController', () => {
     await controller.signUp(dto, '9568be23-16c6-4d87-8dd0-614b34a6c830');
     expect(spy).toBeCalledTimes(1);
   });
-  it('expect signup method executed successfully', async () => {
+  it('expect login method executed successfully', async () => {
     const spy = jest.spyOn(mockedAuthService, 'login').mockImplementation(() => Promise.resolve({ status: 200 }));
     const dto: LoginDTO = plainToInstance(LoginDTO, { password: '1234', username: 'pepito' });
     await controller.login(dto, '9568be23-16c6-4d87-8dd0-614b34a6c830');
     expect(spy).toBeCalledTimes(1);
+  });
+  it('expect an error when login method fails', async () => {
+    jest.spyOn(mockedAuthService, 'login').mockImplementation(() => Promise.reject({ status: 404 }));
+    const dto: LoginDTO = plainToInstance(LoginDTO, { password: '1234', username: 'pepito' });
+    try {
+      await controller.login(dto, '9568be23-16c6-4d87-8dd0-614b34a6c830');
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
+  it('expect an error when signUp method fails', async () => {
+    jest.spyOn(mockedAuthService, 'signUp').mockImplementation(() => Promise.reject({ status: 404 }));
+    const dto: UserDTO = plainToInstance(UserDTO, { email: 'sarasa@gmail.com', password: '1234', username: 'pepito' });
+    try {
+      await controller.signUp(dto, '9568be23-16c6-4d87-8dd0-614b34a6c830');
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
   });
 });
