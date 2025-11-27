de8d8f9e5c286c66a9e209a188aaa3e79c67518f
juan martin di martino <juanma@Macs-MacBook-Pro.local>
2025-11-26 21:13:11 -0300

    cambios

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
 import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
+import { TicketMapper } from './mapper/ticket.mapper';
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket])],
-  providers: [TicketService],
+  providers: [TicketService, TicketMapper],
   controllers: [TicketController]
 })
 export class TicketModule {}
